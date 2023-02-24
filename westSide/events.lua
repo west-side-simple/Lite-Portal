b3426233bf62fab5be3a5634373f304357e42e24
west-side-simple <71137297+west-side-simple@users.noreply.github.com>
2023-02-24 19:47:05 +0200

    + add, fix

--- westSide/events.lua
+++ westSide/events.lua
@@ -1,16 +1,16 @@
---westSide events 02.06.22
+--westSide events 24.02.23
 
 if m_simpleTV.Control.Reason=='addressready'  then
   if m_simpleTV.User.westSide.PortalShowWindowId then
     m_simpleTV.Interface.RemoveExtMenu(m_simpleTV.User.westSide.PortalShowWindowId)
-  end 
+  end
   if m_simpleTV.User.westSide.PortalTable~=nil then
 
     local t={}
     t.utf8 = true
     t.name = '-'
     t.luastring = ''
-    t.lua_as_scr = false 
+    t.lua_as_scr = false
     t.submenu = 'westSide Portal'
     t.imageSubmenu = ''
     --t.key = string.byte('I')
@@ -18,12 +18,12 @@ if m_simpleTV.Control.Reason=='addressready'  then
     t.location = 0
     t.image=''
     m_simpleTV.User.westSide.PortalSeparatorId = m_simpleTV.Interface.AddExtMenuT(t)
-  
+
     local t={}
     t.utf8 = true
     t.name = 'Portal Info Window'
     t.luastring = 'show_portal_window()'
-    t.lua_as_scr = true 
+    t.lua_as_scr = true
     t.submenu = 'westSide Portal'
     t.imageSubmenu = m_simpleTV.MainScriptDir_UTF8 .. 'user/westSide/icons/portal.png'
     t.key = string.byte('I')
@@ -31,14 +31,16 @@ if m_simpleTV.Control.Reason=='addressready'  then
     t.location = 0
     t.image= m_simpleTV.MainScriptDir_UTF8 .. 'user/show_mi/fw_box_t3.png'
     m_simpleTV.User.westSide.PortalShowWindowId = m_simpleTV.Interface.AddExtMenuT(t)
-   
+
   end
 
 end
 
 if m_simpleTV.Control.Reason=='Stopped' or m_simpleTV.Control.Reason=='Error' then
-  m_simpleTV.User.westSide.PortalTable=nil  
-  m_simpleTV.User.filmix.TabSimilar=nil 
+  m_simpleTV.User.westSide.PortalTable=nil
+  m_simpleTV.User.filmix.TabSimilar=nil
+  m_simpleTV.User.torrent.content=nil
+  m_simpleTV.User.hevc.content=nil
   if m_simpleTV.User.westSide.PortalShowWindowId then
     m_simpleTV.Interface.RemoveExtMenu(m_simpleTV.User.westSide.PortalShowWindowId)
   end
