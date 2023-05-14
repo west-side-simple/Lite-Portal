347812799ddae56d0536b1a4be5f08469805dbcd
west-side-simple <71137297+west-side-simple@users.noreply.github.com>
2023-05-14 14:07:53 +0300

    + add new events

--- westSide/events.lua
+++ westSide/events.lua
@@ -1,4 +1,4 @@
---westSide events 24.04.23
+--westSide events 28.04.23
 
 local function getConfigVal(key)
 	return m_simpleTV.Config.GetValue(key,"LiteConf.ini")
@@ -49,13 +49,13 @@ if m_simpleTV.Control.Reason=='Stopped' or m_simpleTV.Control.Reason=='Error' th
 		m_simpleTV.Interface.RemoveExtMenu(m_simpleTV.User.westSide.PortalShowWindowId)
 	end
 end
-if m_simpleTV.Control.Reason=='addressready' and m_simpleTV.Control.CurrentAddress:match('/main/video%d+%.mp4') and m_simpleTV.User.westSide.UP and m_simpleTV.User.westSide.UP == true then
+if m_simpleTV.Control.Reason=='addressready' and (m_simpleTV.Control.CurrentAddress:match('/main/video%d+%.mp4') or m_simpleTV.Control.CurrentAddress:match('/SimpleTVupd/news%d+%.mp4')) and m_simpleTV.User.westSide.UP and m_simpleTV.User.westSide.UP == true then
 	if m_simpleTV.Control.MainMode == 0 then
 		m_simpleTV.Interface.SetBackground({BackColor = 0, PictFileName = 'http://m24.do.am/SimpleTVupd/beckgr.jpg', TypeBackColor = 0, UseLogo = 4, Once = 1})
 	end
 	m_simpleTV.Control.Action = 'stop'
 end
-if m_simpleTV.Control.Reason=='EndReached' and m_simpleTV.Control.CurrentAddress:match('/main/video%d+%.mp4') then
+if m_simpleTV.Control.Reason=='EndReached' and (m_simpleTV.Control.CurrentAddress:match('/main/video%d+%.mp4') or m_simpleTV.Control.CurrentAddress:match('/SimpleTVupd/news%d+%.mp4')) then
 	if m_simpleTV.Control.MainMode == 0 then
 		m_simpleTV.Interface.SetBackground({BackColor = 0, PictFileName = 'http://m24.do.am/SimpleTVupd/beckgr.jpg', TypeBackColor = 0, UseLogo = 4, Once = 1})
 	end
@@ -63,7 +63,7 @@ if m_simpleTV.Control.Reason=='EndReached' and m_simpleTV.Control.CurrentAddress
 end
 if (m_simpleTV.Control.Reason=='Stopped' or m_simpleTV.Control.Reason=='Error' or m_simpleTV.Control.GetPosition() and m_simpleTV.Control.GetPosition()>=0.9)
 	and (m_simpleTV.User.westSide.UP==nil or m_simpleTV.User.westSide.UP~=true)
-	and m_simpleTV.Control.CurrentAddress:match('/main/video%d+%.mp4') then
+	and (m_simpleTV.Control.CurrentAddress:match('/main/video%d+%.mp4') or m_simpleTV.Control.CurrentAddress:match('/SimpleTVupd/news%d+%.mp4')) then
 	if m_simpleTV.Control.MainMode == 0 then
 		m_simpleTV.Interface.SetBackground({BackColor = 0, PictFileName = 'http://m24.do.am/SimpleTVupd/beckgr.jpg', TypeBackColor = 0, UseLogo = 4, Once = 1})
 	end
